1616522a303612d0a92876bb1fbf45c2be0661a9
davidtall <davidtall@qq.com>
2026-08-17 17:21:43 +0800

    编译dockerman

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -136,7 +136,7 @@ if [ -f "$GITHUB_WORKSPACE/Scripts/PRIVATE.sh" ]; then
 fi
 
 #删除官方的默认插件
-rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
+rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dae*,bypass*}
 rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
 
 cp -r $GITHUB_WORKSPACE/package/v2ray-geodata ./
